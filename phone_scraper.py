70b23f598fd0b7453cfde00a08a16f9f01a5e893
AlainDaccache <alaindacc@gmail.com>
2020-03-16 14:26:48 -0400

    question 1, question 2 (two options) done

--- phone_scraper.py
+++ phone_scraper.py
@@ -1,19 +1,26 @@
+from argparse import ArgumentParser
+
 import psycopg2
 import random
 import bs4 as bs
 import requests
 import re
-def make_connection():
+from datetime import timedelta
+from Tools.scripts.treesync import raw_input
+
+# Connect with the database
+try:
     connection = psycopg2.connect(user="cs421g32",
                                   password="KashmirForPakistan98",
                                   host="comp421.cs.mcgill.ca",
                                   port="5432",
                                   database="cs421")
-    return connection
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to Postgres Database", error)
 
 
 def scrape_phones():
-
     resp = requests.get('https://en.wikipedia.org/wiki/Comparison_of_smartphones')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
     product_list = []
@@ -21,8 +28,8 @@ def scrape_phones():
     row_count = 0
     for table in soup.findAll('table', {'class': 'wikitable sortable collapsible'}):
         for row in table.findAll('tr')[1:]:
-            row_count = row_count +1
-            if len(row.findAll('td')) == 17:        # in the website, some rows share the same column data, so
+            row_count = row_count + 1
+            if len(row.findAll('td')) == 17:  # in the website, some rows share the same column data, so
                 # will skip them to make sure each row is "complete"
                 barcode_no = random.randint(100000000000, 999999999999)
                 model = row.findAll('td')[0].text.strip()
@@ -39,11 +46,10 @@ def scrape_phones():
                     continue
                 product_list.append((barcode_no, brand, 0))
                 phone_list.append((barcode_no, cpu, battery, model, storage, ram))
-    return (product_list, phone_list)
+    return product_list, phone_list
 
 
-def main():
-    connection = make_connection()
+def populate_phones():
     postgres_insert_query_product = """ INSERT INTO Product (barcode_no, brand, unit_price) 
                                         VALUES (%s,%s,%s)"""
     postgres_insert_query_phone = """INSERT INTO Phone (barcode_no, cpu, battery, model, storage, ram)  
@@ -65,4 +71,128 @@ def main():
     return 0
 
 
+def find_inactive(months):
+    try:
+        cursor = connection.cursor()
+        postgres_fetch_query_inactive = """SELECT c.email FROM Customer c, Basket b
+                                            WHERE b.email = c.email
+                                            AND (DATE_PART('year', NOW()) - DATE_PART('year', time) ) * 12 
+                                        + (DATE_PART('month', NOW()) - DATE_PART('month', time)) > %s """
+        cursor.execute(postgres_fetch_query_inactive, months)
+        customer_records = cursor.fetchall()
+        if (len(customer_records) == 0):
+            print("No inactive customers emails for the specified period")
+        else:
+            print('Inactive Customers email(s) for the specified period are: ')
+            for row in customer_records:
+                print(row[0])
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching Customer records from PostgreSQL: ", error)
+
+    finally:
+        cursor.close()
+
+def add_phone(brand, price, barcode, cpu, battery, model, storage, ram):
+
+    postgres_insert_query_phone = """INSERT INTO Phone (barcode_no, cpu, battery, model, storage, ram)  
+                                     VALUES (%s, %s, %s, %s, %s, %s)"""
+
+    postgres_insert_query_product = """ INSERT INTO Product (barcode_no, brand, unit_price) 
+                                        VALUES (%s, %s, %s)"""
+
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(postgres_insert_query_product, (barcode, brand, price))
+        connection.commit()
+        cursor.execute(postgres_insert_query_phone, (barcode, cpu, battery, model, storage, ram))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Phone record successfully inserted into the database")
+    except (Exception, psycopg2.Error) as error:
+        print("Error while inserting Phone record into the database: ", error)
+
+    finally:
+        cursor.close()
+
+
+def check_shift_time():
+    try:
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "SELECT eid, start_time, end_time FROM Shift;"
+
+        cursor.execute(postgreSQL_select_Query)
+        print("Selecting rows from shifts table using cursor.fetchall")
+        shift_records = cursor.fetchall()
+
+        print("Print each row and it's columns values")
+
+        for row in shift_records:
+            difference = abs(row[1] - row[2])
+            print(difference)
+            if difference > timedelta(hours = 4):
+                postgreSQL_update_Query = "UPDATE Shift " \
+                                          "SET end_time = (start_time + 4) " \
+                                          "WHERE eid = AND start_time = AND end_time = ;"
+                cursor.execute(postgreSQL_update_Query)
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL: ", error)
+
+    finally:
+        cursor.close()
+
+
+def main():
+        # Scrape the web to populate the Phones relation
+        # populate_phones()
+
+        # Enforce a constraint by searching your database for violations and fixing them in some way.
+        # check_shift_time(connection)
+
+        while True:
+
+            print ("""
+                                    Command                                                     Description
+            ---------------------------------------------------------------------------------------------------------------
+            findInactive t                                                          Look up emails of customers inactive for t months
+            addPhone brand price barcode_no cpu battery model storage ram    Add a phone to the database
+            Ketan
+            Aakarsh
+            Shayan
+            exit                                                                    Exit program
+            """)
+
+            ans = raw_input("What would you like to do? ").split()
+
+            if ans[0] == "findInactive" and len(ans) == 2:
+                find_inactive(ans[1])
+
+            elif ans[0] == "addPhone" and len(ans) == 9:
+                add_phone(ans[1], ans[2], ans[3], ans[4], ans[5], ans[6], ans[7], ans[8])
+
+            elif ans[0] == "Ketan":
+                print("Ketan's Part")
+
+            elif ans[0] == "Aakarsh":
+                print("Aakarsh's Part")
+
+            elif ans[0] == "Shayan":
+                print("Shayan's Part")
+
+            elif ans[0] == "exit":
+                connection.close()
+                exit()
+
+            elif ans != "":
+                print("\n Not Valid Choice Try again")
+
 main()
+
+'''
+Sample Commands:
+
+addPhone Apple 130 1234543 42 4200 iPhoneX 64 32
+
+'''
--- phone_store.sql
+++ phone_store.sql
@@ -19,6 +19,7 @@ CREATE TABLE Basket (
                         order_num INTEGER NOT NULL,
                         order_total INTEGER NOT NULL,
                         email VARCHAR NOT NULL,
+                        time TIMESTAMP DEFAULT NOW(),
                         PRIMARY KEY (order_num),
                         Foreign KEY (email) REFERENCES Customer(email)
 );
@@ -26,14 +27,14 @@ CREATE TABLE Basket (
 CREATE TABLE Product (
                          barcode_no VARCHAR NOT NULL,
                          brand VARCHAR NOT NULL,
-                         color VARCHAR NOT NULL,
+                         color VARCHAR,
                          unit_price FLOAT NOT NULL,
                          order_num INT,
                          PRIMARY KEY (barcode_no),
                          foreign key (order_num) REFERENCES Basket(order_num)
 );
 
-CREATe table Employees (
+CREATE table Employees (
                            eid INT NOT NULL,
                            home_address VARCHAR NOT NULL,
                            salary FLOAT NOT NULL,
@@ -107,13 +108,13 @@ The constraints that are not shown are
 - The spinoff Employees =Manager=> Supervision -salesperson- Employees
 - Some participation constraints. In fact, in relationships, it is possible
   to specify if there is a primary key constraint. For example, since the existence of
-  a product isn"t dependent on the existence of an order, we should be able to insert a
+  a product isn't dependent on the existence of an order, we should be able to insert a
   product tuple without necessarily entering a order_id. Therefore, order_id, a foreign key
   to order, should not be specified NOT NULL in Product. If we do, then we should always specify
-  an order id. However, this means that we cannot differentiate between an "exactly one"
-  and "at least one".
+  an order id. However, this means that we cannot differentiate between an 'exactly one'
+  and 'at least one'.
 
-  It is possible to add the same phone to two different orders without warning? TEST
+  It is possible to add the same phone to two different orders without warning?
 
 */
 
@@ -121,17 +122,17 @@ The constraints that are not shown are
 -- DESCRIBE tablename (ii) for PostgreSQL use \d table name (prints the description of the relation on the
 -- screen) for each of your relations, print the result and turn it in
 
-/*
-\d Stock;
-\d Warehouse;
-\d Shift;
-\d Employees;
-\d Phone;
-\d Accessories;
-\d Product;
-\d Basket;
-\d Customer;
-*/
+
+-- \d Stock;
+-- \d Warehouse;
+-- \d Shift;
+-- \d Employees;
+-- \d Phone;
+-- \d Accessories;
+-- \d Product;
+-- \d Basket;
+-- \d Customer;
+
 
 
 /* Part Three:
@@ -142,11 +143,11 @@ when you type the INSERT commands.
 
 INSERT INTO Product (barcode_no, color, unit_price, brand)
 VALUES
-("03243234543", "Gold", 699.99, "Apple"),
-("23456787654", "White", 999.99, "Samsung"),
-("87654567876", "Black", 39.99, "Apple"),
-("54323456722", "Transparent", 15.99, "Samsung"),
-("23456789876", "Pink", 399.99, "Huawei");
+('03243234543', 'Gold', 699.99, 'Apple'),
+('23456787654', 'White', 999.99, 'Samsung'),
+('87654567876', 'Black', 39.99, 'Apple'),
+('54323456722', 'Transparent', 15.99, 'Samsung'),
+('23456789876', 'Pink', 399.99, 'Huawei');
 
 -- Print and turn in the result when you issue a SELECT * FROM relationname command.
 -- SELECT *
@@ -162,59 +163,67 @@ data into DB2 tables is provided on my courses
 
 INSERT INTO Customer(email)
 VALUES
-("alain.daccache@mail.mcgill.ca"),
-("aakarsh.shekhar@mail.mcgill.ca"),
-("ketan.rampurkar@mail.mcgill.ca"),
-("shayan.sheikh@mail.mcgill.ca"),
-("hisham.hawara@mail.mcgill.ca");
+('alain.daccache@mail.mcgill.ca'),
+('aakarsh.shekhar@mail.mcgill.ca'),
+('ketan.rampurkar@mail.mcgill.ca'),
+('shayan.sheikh@mail.mcgill.ca'),
+('hisham.hawara@mail.mcgill.ca');
 
 INSERT INTO Employees(eid, salary, home_address)
 VALUES
-(1, 1000, "425 Backlemore Avenue, Montreal, Canada"),
-(2, 2000, "843 Sherbrooke Street, Montreal, Canada"),
-(3, 3000, "521 Milton Street, Montreal, Canada");
+(1, 1000, '425 Backlemore Avenue, Montreal, Canada'),
+(2, 2000, '843 Sherbrooke Street, Montreal, Canada'),
+(3, 3000, '521 Milton Street, Montreal, Canada');
 
 INSERT INTO Shift(date, start_time, end_time, shift_responsibility, eid)
 VALUES
-("02/22/2020", "2020-02-22 19:00:00-07", "2020-02-22 21:00:00-07", "Cleanup", 1),
-("02/23/2020", "2020-02-22 16:00:00-07", "2020-02-22 19:00:00-07", "On help desk", 2),
-("02/22/2020", "2020-02-22 12:00:00-07", "2020-02-22 14:00:00-07", "On sales", 3);
+('02/21/2020', '2020-02-21 19:00:00-07', '2020-02-21 21:00:00-07', 'Cleanup', 1),
+('02/22/2020', '2020-02-22 16:00:00-07', '2020-02-22 22:00:00-07', 'On help desk', 2),
+('02/22/2020', '2020-02-22 12:00:00-07', '2020-02-22 14:00:00-07', 'On sales', 3),
+('03/22/2020', '2020-03-22 12:00:00-07', '2020-03-22 20:00:00-07', 'On sales', 1)
+;
 
 INSERT INTO Phone (barcode_no, cpu, battery, model, storage, ram)
 VALUES
-("03243234543", "ee", 3240, "0372A", 64, 16),
-("23456787654", "ee", 4120, "23BCA", 32, 8),
-("23456789876", "ee", 2350, "52XBY", 16, 4);
+('03243234543', 'ee', 3240, '0372A', 64, 16),
+('23456787654', 'ee', 4120, '23BCA', 32, 8),
+('23456789876', 'ee', 2350, '52XBY', 16, 4);
 
 INSERT INTO Accessories(barcode_no, type)
 VALUES
-("54323456722", "Samsung Screen Protector"),
-("87654567876", "iPhone Lightning to USB Charging Cable");
+('54323456722', 'Samsung Screen Protector'),
+('87654567876', 'iPhone Lightning to USB Charging Cable');
 
-INSERT INTO Basket (order_num, order_total, email)
+INSERT INTO Basket (order_num, order_total, email, time)
 VALUES
-(001, 2, "alain.daccache@mail.mcgill.ca"),
-(002, 1, "ketan.rampurkar@mail.mcgill.ca");
+(001, 2, 'alain.daccache@mail.mcgill.ca', NOW()),
+(002, 1, 'ketan.rampurkar@mail.mcgill.ca', '2018-01-08 04:05:06');
 
 UPDATE Product
 SET order_num = 001
-WHERE barcode_no = "03243234543";
+WHERE barcode_no = '03243234543';
 
 UPDATE Product
 SET order_num = 001
-WHERE barcode_no = "54323456722";
+WHERE barcode_no = '54323456722';
 
 UPDATE Product
 SET order_num = 002
-WHERE barcode_no = "87654567876";
+WHERE barcode_no = '87654567876';
+
 
 -- For each table show the output, truncated to the first 5-10 tuples, that are returned when you
 -- issue a SELECT * FROM relationname command
-/* SELECT * FROM Product LIMIT 5;
+SELECT * FROM Product LIMIT 5;
 SELECT * FROM Phone LIMIT 5;
 SELECT * FROM Accessories LIMIT 5;
 SELECT * FROM Basket LIMIT 5;
-SELECT * FROM Customer LIMIT 5; */
+SELECT * FROM Customer LIMIT 5;
+SELECT * FROM Shift LIMIT 5;
+SELECT * FROM Stock LIMIT 5;
+SELECT * FROM Warehouse LIMIT 5;
+SELECT * FROM Employees LIMIT 5;
+
 
 /* Part Five
 Write five queries on your project database, using the select-from-where construct of SQL. The
@@ -222,7 +231,7 @@ queries should be typical queries of the application domain. To receive full cre
 your queries must exhibit some interesting feature of SQL: queries over more than one relation, subqueries,
 aggregations, grouping etc.
 Turn in a description of all the relations that you use in your queries (e.g., the original create
-statements or printouts from the SQL “describe table yourname” function), a description of
+statements or printouts from the SQL â€œdescribe table yournameâ€ function), a description of
 what each of your queries is supposed to do, the SQL statement of each query, along with a
 script illustrating their execution (for example the screenshot when you execute the query).
 Your script should be sufficient to convince us that your commands run successfully. Please do
@@ -260,7 +269,7 @@ SELECT eid
 FROM Employees
 WHERE eid IN (SELECT eid
               FROM Shift
-              WHERE date = "02/22/2020");
+              WHERE date = '02/22/2020');
 
 -- Trivial query
 -- List the phone models that have a RAM over 8 GB
@@ -270,7 +279,7 @@ WHERE Phone.ram > 8;
 
 /* Part Six
 Write four data modification commands for your application. Most of these commands should
-be “interesting,” in the sense that they involve some complex feature, such as inserting the result of a query,
+be â€œinteresting,â€ in the sense that they involve some complex feature, such as inserting the result of a query,
 updating several tuples at once, or deleting a set of tuples that is more than one but less than all the tuples in
 a relation.
 Turn in a description of all the relations that you use in your modifications but are not described
@@ -278,6 +287,84 @@ so far. Provide a short description of what each of your statements is supposed 
 SQL statements themselves and a script or screenshot that shows your modification commands
 running in a convincing fashion.
 */
+
+--update several tuples at once
+-- Update the Samsung and Apple brand phones/accessories price due to 20% and 10% discount respectively on them
+UPDATE Product
+SET unit_price=(case when brand='Samsung' then unit_price*0.8
+                     when brand='Apple' then unit_price*0.9
+    end)
+where brand in ('Samsung', 'Apple');
+
+--insert result of a query
+-- Insert the manager of the warehouse using the employee id in employees relation
+insert into warehouse(address, eid)
+select '3475 Rue Durocher, Montreal, Canada', eid
+from employees
+where eid=2;
+
+--Change the shift responsibility to cleanup for eid={1,2,3,4}
+--update several tuples at once
+update shift
+set shift_responsibility='Cleanup'
+where eid in (1,2,3,4);
+
+--Update the phone storage of the maximum bar code value of phone
+update phone
+set storage=64
+where barcode_no=(select max(barcode_no) from phone);
+
+/* Part 7: Create two views on top of your database schema.
+Turn in an informal description what data each of the views represents, show your CREATE
+VIEW statements and the response of the system. Also, show a query involving each view and
+the system response (but truncate the response if there are more than a few tuples produced).
+Finally, show a script of what happens when you try run an SQL UPDATE statement on each of
+your views. Are either of your views updatable (that is, the database system will automatically
+translate the update into an update on the base table(s). Explain why or why not. Summarize
+the conditions that must hold so that DB2/PostgreSQL allows updating a view.*/
+
+--Products that are demanded by the customers currently
+CREATE VIEW ProductsDemanded (barcode_no, brand, color, unit_price, order_num)
+AS SELECT * FROM Product
+   WHERE order_num IS NOT NULL;
+
+
+SELECT * FROM ProductsDemanded;
+
+--List of employees who are on the cleanup duty for a particular shift
+CREATE VIEW CleanupDutyStaff (date, start_time, end_time, shift_responsibility, eid)
+AS SELECT * from Shift
+   WHERE shift_responsibility='Cleanup';
+
+SELECT * FROM CleanupDutyStaff;
+
+UPDATE ProductsDemanded SET order_num=1 where ProductsDemanded.barcode_no='03243234543';
+update CleanupDutyStaff set eid=1  where CleanupDutyStaff.start_time='2020-02-22 19:00:00.000000' and CleanupDutyStaff.end_time='2020-02-22 21:00:00.000000';
+
+drop VIEW ProductsDemanded;
+drop VIEW CleanupDutyStaff;
+
+/* Part 8
+ Add two CHECK constraints to relations of your database schema.
+Turn in the revised schema, its successful declaration, and the response of database
+ to modifications (insert/update) that violate the constraints.
+--  */
+
+-- Inserting in-correct values
+INSERT INTO Employees(eid, salary, home_address)
+VALUES
+(4, -1500, '320 rue Sherbrooke O., Montreal, Canada ');
+
+INSERT INTO Shift(date, start_time, end_time, shift_responsibility, eid)
+VALUES
+('02/22/2020', '2020-02-22 13:00:00-07', '2020-02-22 10:00:00-07', 'Customer assistance', 4);
+
+-- Revising the schema
+ALTER TABLE employees
+    ADD CONSTRAINT valid_salary_check CHECK (salary >= 0);
+
+ALTER TABLE shift
+    ADD CONSTRAINT valid_time_check CHECK (end_time > start_time);
 
 -- DROP TABLE Stock;
 -- DROP table Warehouse;
